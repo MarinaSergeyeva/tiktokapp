7feb139a863ce0382385ff6563bbbe07aafeebc4
MarinaSergeyeva <morrrinad@gmail.com>
2021-11-17 20:42:05 +0200

    home pagination implemented

--- src/components/PostLIst/index.js
+++ src/components/PostLIst/index.js
@@ -1,16 +1,50 @@
-import React from 'react';
-import { Stack } from '@mui/material';
+import React, { useState } from 'react';
+import { Divider, Pagination, Stack } from '@mui/material';
+import { Box } from '@mui/system';
 import { PostItem } from '../PostItem';
-import CustomPagination from '../Pagination';
 
-export const PostList = ({ data }) => (
-  <Stack
-    direction="row"
-    display="flex"
-    flexWrap="wrap"
-    justifyContent="space-evenly"
-    pt="70px"
-  >
-    <CustomPagination data={data} itemComponent={<PostItem />} />
-  </Stack>
-);
+export const PostList = ({ data }) => {
+  const itemsPerPage = 6;
+  const [page, setPage] = useState(1);
+  const [numOfPages] = useState(data.length / itemsPerPage);
+
+  const handleChange = (e, value) => {
+    setPage(value);
+  };
+
+  return (
+    <Stack>
+      <Stack
+        direction="row"
+        display="flex"
+        flexWrap="wrap"
+        justifyContent="space-evenly"
+        pt="70px"
+      >
+        {data
+          .slice((page - 1) * itemsPerPage, page * itemsPerPage)
+          .map(item => (
+            <PostItem key={item.id} item={item} />
+          ))}
+      </Stack>
+      {data.length > 6 && (
+        <Stack>
+          <Divider sx={{ mb: '10px' }} />
+          <Box component="span">
+            <Pagination
+              count={numOfPages}
+              page={page}
+              onChange={handleChange}
+              defaultPage={1}
+              color="primary"
+              size="large"
+              showFirstButton
+              showLastButton
+              sx={{ display: 'flex', justifyContent: 'center' }}
+            />
+          </Box>
+        </Stack>
+      )}
+    </Stack>
+  );
+};
